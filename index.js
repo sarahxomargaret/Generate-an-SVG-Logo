07b4d229272800c838702f41539599a8d52fa077
Fawn <sarahmargaret@ymail.com>
2023-05-24 14:41:59 -0400

    added questions

--- index.js
+++ index.js
@@ -5,5 +5,34 @@ const questions = [
         message: 'Pick up to three characters',
         name: 'text',
         maxLength: '3',
-    }
+    },
+    {
+        type: 'input',
+        message: 'What color do you want the text?',
+        name: 'color',
+    },
+    {
+        type: 'list',
+        message: 'Please choose a shape for your logo',
+        name: 'shape',
+        choices: [
+            {
+                name: 'Circle',
+                value: 'circle',
+            },
+            {
+                name: 'Square',
+                value: 'square',
+            },
+            {
+                name: 'Triangle',
+                value: 'triangle',
+            }
+        ]
+    },
+    {
+        type: 'input',
+        message: 'What color do you want your shape to be?',
+        name: 'backgroundcolor',
+    },
 ]
